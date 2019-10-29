e637c4433fb9d95b6d164d6a629c7449f9194437
David Tauraso <david318@me.com>
2019-10-29 11:52:22 -0700

    Put in plan for events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+/*
+1
+make signup buttons enlarge and shrink
+no propigation(reguarding event 5)
+
+2
+change text color for nat items
+
+3
+have bus image "dance"
+
+4 run a doubleclick when ceratain passages are clicked on
+
+5
+hilight entire sign up prompt with a color when mousing over it
+(make sure this doesn't interfeer with the button clicks)
+
+6
+rotate funbus "icon"
+
+7 change background color for footer
+
+8
+have the destination image change to the buss image when clicked
+
+9
+show another link when at a certain place in the nav bar
+
+10 change text to lorem lala for a single passage when clicked
+*/
+
